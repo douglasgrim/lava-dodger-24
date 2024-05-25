c773c2f3303053124c2b0946d824260781d254c0
Douglas Grim <douglas.grim.123@gmail.com>
2024-05-25 12:35:01 -0700

    moved getmapposition into new file

--- src/webview/utils/index.ts
+++ src/webview/utils/index.ts
@@ -1,40 +1,4 @@
 import { getImage } from './getImage';
+import { getMapPosition } from './getMapPosition';
 
-export type GridPlacement = {
-  x: number,
-  y: number,
-  mapWidth: number,
-  mapHeight: number,
-  mapXOffset?: number,
-  mapYOffset?: number,
-  gridWidth?: number,
-  gridHeight?: number,
-}
-
-export type MapPosition = {
-  mapX: number,
-  mapY: number,
-  heroX: number,
-  heroY: number,
-}
-
-export const getMapPosition = ({
-  x,
-  y,
-  mapWidth,
-  mapHeight,
-  gridWidth = 54,
-  gridHeight = 54,
-  mapXOffset = 2,
-  mapYOffset = 2
-}: GridPlacement): MapPosition => {
-  console.log(mapHeight, mapWidth, x, y);
-  const heroX = x * gridWidth + mapXOffset;
-  const heroY = y * gridHeight + mapYOffset;
-  const mapX = Math.max(0, heroX - mapWidth / 2);
-  const mapY = Math.max(0, heroY - mapHeight / 2);
-
-  return { mapX, mapY, heroX, heroY };
-}
-
-export { getImage };
+export { getImage, getMapPosition };
