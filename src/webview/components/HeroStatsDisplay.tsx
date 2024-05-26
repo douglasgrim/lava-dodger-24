538d9eff9e3bad5a118628dfad33c4dcd300a460
Doug <douglas.grim.123@gmail.com>
2024-05-25 23:23:43 -0700

    styling of hero stats

--- src/webview/components/HeroStatsDisplay.tsx
+++ src/webview/components/HeroStatsDisplay.tsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
 import './sidebars.css';
+import { RootState } from '../../app/state/store';
 
 const HeroStatsDisplay = () => {
-    return (
-        <div className="sidebars">
-            <h1>Hero Stats</h1>
-        </div>
-    )
+  const { heroStatus } = useSelector((state: RootState) => state.gameComponents);
+  return (
+    <div className="sidebars">
+      <h1>Hero Stats</h1>
+      <div>{JSON.stringify(heroStatus)}</div>
+    </div>
+  )
 }
 
 export default HeroStatsDisplay;
